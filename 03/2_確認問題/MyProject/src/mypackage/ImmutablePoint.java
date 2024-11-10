f18690c40f33e2d6406d603a52ce93eb58d0b457
yuukisakai54 <yuukisakai54@gmail.com>
2024-11-10 14:36:33 +0900

    Java_Basic 03/1 lesson add. Java_Basic 03/2 lesson add.

---
+++ 03/2_確認問題/MyProject/src/mypackage/ImmutablePoint.java
@@ -0,0 +1,19 @@
+package mypackage;
+
+public class ImmutablePoint {
+	private int x;
+	private int y;
+	
+	public ImmutablePoint(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+}
