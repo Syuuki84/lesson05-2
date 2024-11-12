81763869008128fa91e78e5444ee19cb15b521e0
yuukisakai54 <yuukisakai54@gmail.com>
2024-11-12 23:08:12 +0900

    Add Main program and ShoppingCart application with Product and ShoppingCart classes

---
+++ MyProject2/src/mypackage/ShoppingCart.java
@@ -0,0 +1,29 @@
+package mypackage;
+
+import java.util.ArrayList;
+
+public class ShoppingCart {
+    private ArrayList<Product> items;
+    private static final int CART_CAPACITY = 10;
+ 
+    public ShoppingCart() {
+        items = new ArrayList<>();
+    }
+
+   
+    public void addItem(Product product) { 
+    	if (items.size() < CART_CAPACITY) { 
+    		items.add(product);
+    } else { 
+    	System.out.println("カートがいっぱいです。商品を追加できません。"); }
+    }
+
+    
+    public double getTotalPrice() {
+        double total = 0.0;
+        for (Product item : items) {
+        	total += item.getPrice();
+        }
+        return total;
+    }
+}
